31b20f6a316b74b53019b674d4c04d836289b92d
solooo <pjian108@gmail.com>
2017-07-13 17:49:00 +0800

    添加 generator 自动生成 mybatis代码

---
+++ springboot-mybatis/src/main/java/net/solooo/template/controller/IndexController.java
@@ -0,0 +1,14 @@
+package net.solooo.template.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description:
+ * Author:Eric
+ * Date:2017/7/13
+ */
+@Controller
+@RequestMapping(value = "/", produces = {"application/json;charset=utf-8"})
+public class IndexController {
+}
